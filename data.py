166102d65a30a5f3fc9674910ee6d781b74d52ed
feelingproudindianarmy <celestialh4@gmail.com>
2024-09-27 19:20:33 +0545

    data types

---
+++ data.py
@@ -0,0 +1,11 @@
+
+a=10
+print("type of a is:" , type(a))
+b=1.5
+print("type of b is:" , type(b))
+c="Pranshu"
+print("type of c is:" , type(c))
+d=True
+print("type of d is:" , type(d))
+b=int(b)
+print("type of b is:" , type(b))
